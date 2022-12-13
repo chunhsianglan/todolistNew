5b401d8d743c663d6cabe3afd99af21e1ba7d86a
chunhsianglan <nomo.webdesign@gmail.com>
2022-12-13 19:01:28 +0000

    update

--- js/all.js
+++ js/all.js
@@ -2,67 +2,70 @@
 // 2. TodoList: 待辦事項會有狀態（完成與否），可透過 checkbox 來切換。
 // 3. TodoList: 待辦清單列表會有『全部』、『完成』、『未完成』Tab 來做篩選切換。
 // 4. TodoList: 可以清除全部已完成功能
-const addText = document.querySelector('.addText');
-const addBtn = document.querySelector('.addBtn');
-const list_bar = document.querySelector('.list_bar');
-const check_btn = document.querySelector('.check_btn');
-const delete_all_btn = document.querySelector('.delete_all_btn');
+const addText = document.querySelector(".addText");
+const addBtn = document.querySelector(".addBtn");
+const list_bar = document.querySelector(".list_bar");
+const check_btn = document.querySelector(".check_btn");
+const delete_all_btn = document.querySelector(".delete_all_btn");
 
 var data = [
-    {
-        "content":"Clean the bathroom"
-    },
-    {
-        "content":"Paint the wall"
-    }
-]
+  {
+    content: "Clean the bathroom",
+  },
+  {
+    content: "Paint the wall",
+  },
+];
 
-function renderData(){
-
-var str = "";
-data.forEach(function(item){
-    str+=`<li class="list">
+ function renderData() {
+  var str = "";
+  data.forEach(function (item) {
+    str += `<li class="list">
     <input type="checkbox" class="check_btn" /><span
       >${item.content}</span
-    ><input type="button" class="delete_list" value="Delete" />`
-})
-
-list_bar.innerHTML = str;
+    ><input type="button" class="delete_list" value="Delete" /></li>`
+  })
+  
+  list_bar.innerHTML = str;
 }
 
-addBtn.addEventListener('click',function(e){
-    if(addText.value == ""){
-        alert('請填寫資訊');
-        return
-    }
-    var obj = {};
-    obj.content = addText.value;
-    data.push(obj);
-    renderData();
-    addText.value = "";
+addBtn.addEventListener("click", function (e) {
+  if (addText.value == "") {
+    alert("請填寫資訊");
+    return
+  }
+  var obj = {};
+  obj.content = addText.value;
+  data.push(obj);
+  renderData();
+  addText.value = "";
+  
 })
 
-data.forEach(function(item,index){
-    var str="";
-    str+=`<li class="list">
+
+data.forEach(function (item, index) {
+  var str = "";
+  str += `<li class="list">
     <input type="checkbox" class="check_btn" /><span
       >${item.content}</span
     ><input type="button" class="delete_list" data-num=${index} value="Delete" />`;
-})
+});
 
+list_bar.addEventListener("click", function (e) {
+  if (e.target.getAttribute("class") == "delete_list") {
+    var num = e.target.getAttribute("data-num");
+    data.splice(num,1);
+    renderData();
+  }
+});
 
-list_bar.addEventListener('click',function(e){
-    if(e.target.getAttribute('class') == 'delete_list'){
-        var num = e.target.getAttribute('data-num');
-        data.splice(num,1);
-        renderData();
-    }
-})
+// //checkbox打勾＆點到delete all btn的話，刪除list
 
-//checkbox打勾＆點到delete all btn的話，刪除list
-
-delete_all_btn.addEventListener('click',function(e){
-    if(e.target.value == "on" && e.target.getAttribute('class') == "delete_all_btn"){
-        list_bar.innerHTML = "";
-    } 
-})
+// delete_all_btn.addEventListener("click", function (e) {
+//   if (
+//     e.target.value == "on" &&
+//     e.target.getAttribute("class") == "delete_all_btn"
+//   ) {
+//     list_bar.innerHTML = "";
+//   }
+// });
